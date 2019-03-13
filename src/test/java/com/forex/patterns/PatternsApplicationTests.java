a5e79cd1e26dc0d46fd4e8e0210a13fd0b72d359
Cemal Türkoglu <cturkoglu@handsonsystems.co.uk>
2019-03-13 17:50:27 +0100

    test for rest consumer and find local min function

--- src/test/java/com/forex/patterns/PatternsApplicationTests.java
+++ src/test/java/com/forex/patterns/PatternsApplicationTests.java
@@ -1,11 +1,21 @@
 package com.forex.patterns;
 
+import com.forex.patterns.model.Bar;
+import com.forex.patterns.model.Currency;
+import com.forex.patterns.model.ExchangeRateData;
+import com.forex.patterns.model.Interval;
 import com.forex.patterns.util.RestConsumer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.omg.CORBA.INTERNAL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.forex.patterns.util.Extremum.findLocalMinimums;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,10 +25,29 @@ public class PatternsApplicationTests {
     RestConsumer restConsumer;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws IOException {
 
-        restConsumer.retrieveExchangeData();
+        List<Bar> exchangeRateData = restConsumer.retrieveExchangeData(
+                Currency.EUR,
+                Currency.USD,
+                Interval._60MIN
+        );
 
-	}
+        List<Bar> localMinimums = findLocalMinimums(exchangeRateData, 5);
+
+        System.out.println("size="+exchangeRateData.size());
+
+
+        System.out.println("Local mins");
+
+        for(Bar bar: localMinimums){
+            System.out.println(bar.toString());
+        }
+
+        System.out.println("size="+localMinimums.size());
+
+
+
+    }
 
 }
